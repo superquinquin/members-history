fea56c6892ccdc82b94b1d048837e1350f789bcb
Remi Nivet <rnivet@gmail.com>
2026-01-05 17:13:59 +0100

    cleanup before push

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Geocoding scripts for cooperative members.
-
-This package contains utilities for geocoding member addresses
-using the French government geocoding API with rate limiting.
-"""
-
-__version__ = "1.0.0"
